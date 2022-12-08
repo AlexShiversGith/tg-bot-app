b9fdbba9e6e01f8a1130e08fa0c0fb1cab99a690
AlexShiversGith <alex.shivers.gith@gmail.com>
2022-12-08 16:23:53 +0300

    add

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { useTelegram } from './hooks/useTelegram';
 import Header from './components/header/Header';
 
 function App() {
-  const {onToggleButton, tg} = useTelegram()
+  const {onToggleButton, tg} = useTelegram();
 
   useEffect(() => {
     tg.ready();
-  }, [])
+  }, []);
 
   return (
     <div className="App">
