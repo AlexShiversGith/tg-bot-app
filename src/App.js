80562b977488ba0351ca80fedd9c9af94138ba2e
AlexShiversGith <alex.shivers.gith@gmail.com>
2022-12-08 12:57:31 +0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Button from './components/button/Button';
+import Header from './components/header/Header';
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
+      <Button />
         its worked
     </div>
   );
