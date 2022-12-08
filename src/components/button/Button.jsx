c018729118dad5460c1904d6c5f09cc8a42553dd
AlexShiversGith <alex.shivers.gith@gmail.com>
2022-12-08 13:07:06 +0300

    +

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = () =>{
     return (
-        <button {...props} className={'button' = props.className}/>
+        <button {...props} className={'button ' + props.className}/>
     )
 }
 
