92b00525cf9432cb42f01a9f0ef5a29a97648bde
AlexShiversGith <alex.shivers.gith@gmail.com>
2022-12-09 19:42:51 +0300

    addForms

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,13 +1,59 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useTelegram } from "../../hooks/useTelegram";
 import './Form.css';
 
 const Form = () => {
+    const [country, setCountry] = useState();
+    const [street, setStreet] = useState();
+    const [subject, setSubject] = useState();
+    const {tg} = useTelegram()
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Send data'
+        })
+    },[])
+
+    useEffect(() => {
+        if (!street || !country){
+            tg.MainButton.hide()
+        } else {
+            tg.MainButton.show()
+        }
+    },[country, street])
+
+    const onChangeCountry = () => {
+        setCountry(e.target.value)
+    }
+    const onChangeStreet = () => {
+        setStreet(e.target.value)
+    }
+    const onChangeSubject = () => {
+        setSubject(e.target.value)
+    }
+
+
     return (
         <div className="form">
             <h3>Enter your data</h3>
-            <input className="input" type="text" placeholder={'Country'} />
-            <input className="input" type="text" placeholder={'Street'} />
-            <select className="select">
+            <input 
+                className="input" 
+                type="text" 
+                placeholder={'Country'} 
+                value={country}
+                onChange={onChangeCountry}/>
+            <input 
+                className="input" 
+                type="text" 
+                placeholder={'Street'} 
+                value={street}
+                onChange={onChangeStreet}/>
+            <select 
+                value={subject}
+                onChange={onChangeSubject}
+                className="select">
                 <option value={'physical'}>Physical face</option>
                 <option value={'legal'}>Legal face</option>
             </select>
